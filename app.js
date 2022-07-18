e37d473f60c830d84d9efec9232562318ce1ea1e
Cecilia Ducos <cecilia.ducos@progleasing.com>
2022-07-18 19:40:25 -0300

    Post methods working

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var express = require('express');
-var app = express();
+const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  return res.send('ude-api is running correctly!');
+});
 
 app.get('/problem', (req, res) => {
   return res.send('Received a GET HTTP method');
 });
 
 app.post('/problem', (req, res) => {
-  return res.send('Received a POST HTTP method /problem');
+  return res.send('Received a POST HTTP method');
 });
 
 app.post('/generateAnswer', (req, res) => {
